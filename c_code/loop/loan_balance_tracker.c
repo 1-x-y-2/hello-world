ec5e9a02a95f75a17ce62bf249b8075aa268ab85
离昱 <1927305259@qq.com>
2026-04-16 11:20:01 +0800

    Update loan_balance_tracker.c

--- c_code/loop/loan_balance_tracker.c
+++ c_code/loop/loan_balance_tracker.c
@@ -3,12 +3,13 @@
 int main() {
     float money;
     scanf("%f", &money);
-    float rate = .0f;
+    float rate;
     scanf("%f", &rate);
     float back_money;
     scanf("%f", &back_money);
-    int count;
+    int count = 0;
     scanf("%d", &count);
+
     for (int i = 0; i < count; i++) {
         money = money - back_money + money * rate * .01f / 12.0f;
         printf("%.2f\n", money);
