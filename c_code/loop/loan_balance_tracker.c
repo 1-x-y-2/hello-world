42ecd8c627ac231edb39e496e29832ff232f8d2a
离昱 <1927305259@qq.com>
2026-04-16 11:03:28 +0800

    Add files via upload

---
+++ c_code/loop/loan_balance_tracker.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    float money = .0f;
+    scanf("%f", &money);
+    float rate = .0f;
+    scanf("%f", &rate);
+    float back_money = .0f;
+    scanf("%f", &back_money);
+    int count = 0;
+    scanf("%d", &count);
+    for (int i = 0; i < count; i++) {
+        money = money - back_money + money * rate * .01f / 12.0f;
+        printf("%.2f\n", money);
+    }
+    return 0;
+}
