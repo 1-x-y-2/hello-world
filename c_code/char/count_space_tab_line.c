f60a907a2083beacb63ff4b1be1dbb0d6150b065
离昱 <1927305259@qq.com>
2026-04-17 10:03:16 +0800

    Create count_space_tab_line.c

---
+++ c_code/char/count_space_tab_line.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    int c;
+    double n_space = 0, n_tab = 0, line_n = 0;
+    while ((c = getchar()) != EOF)
+        if (c == '\n')
+            ++line_n;
+        else if (c == '\t')
+            ++n_tab;
+        else if (c == ' ')
+            ++n_space;
+
+    printf("%.0f\n", n_space);
+    printf("%.0f\n", n_tab);
+    printf("%.0f\n", line_n);
+    return 0;
+}
