65f624d6aeb2c463a7b0f83b90669d0b61d5c691
xy <1927305259@qq.com>
2026-04-20 13:25:10 +0800

    修改main.c

--- main.c
+++ main.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
 
-int main(void) {
+int lower(char c) {
+  if (c >= 'A' && c <= 'F') {
+    return c + 'a' - 'A';
+  } else {
+    return c;
+  }
+}
 
-  printf("Hi");
+long add_two_num(char c, long n) {
+  int d = (int)(c - 'a' + 10);
+  int g = d % 10;
+  int s = d / 10;
+  n = 10 * n + s;
+  n = 10 * n + g;
+  return n;
+}
+
+long htoi(char s[]) {
+  long n = 0;
+
+  for (int i = 0; s[i] != '\0'; ++i) {
+    if (s[i] >= 'a' && s[i] <= 'f' || s[i] >= 'A' && s[i] <= 'Z') {
+      s[i] = (char)lower(s[i]);
+    }
+  }
+  return n;
+}
+
+int main(void) {
+  char s[] = {'A', 'B', 'C', '\0'};
+  printf("%ld\n", htoi(s));
   return 0;
 }
