0499c5a95aa49c5059d31a05f41592b2567234bb
xy <1927305259@qq.com>
2026-04-21 13:15:03 +0800

    修改main.c写练习2-5

--- main.c
+++ main.c
@@ -1,34 +1,23 @@
 #include <stdio.h>
 
-int judge(char c, char s[]) {
+int exist(char c, char s[]) {
   for (int i = 0; s[i] != '\0'; i++) {
-    if (s[i] == c)
-      return 0;
-  }
-  return 1;
-}
-
-void my_squeeze(char s1[], char s2[]) {
-  int i, j;
-  for (i = j = 0; s1[i] != '\0'; i++) {
-    if (judge(s1[i], s2)) {
-      s1[j++] = s1[i];
+    if (c == s[i]) {
+      return 1;
     }
   }
-  s1[j] = '\0';
+  return 0;
 }
 
-void show(char s[]) {
-  for (int i = 0; s[i] != '\0'; i++) {
-    printf("%c", s[i]);
+int any(char s1[], char s2[]) {
+  for (int i = 0; s1[i] != '\0'; i++) {
   }
+  return -1;
 }
 
-int main(void) {
-  char s1[] = "abcdefg";
-  char s2[] = "fg";
+int main() {
+  char s1[] = "Hello World";
+  char s2[] = "eH";
 
-  my_squeeze(s1, s2);
-  show(s1);
   return 0;
 }
