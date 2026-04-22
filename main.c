979b22bd5d9d3a2b57cbab6dfbbb0d04436cf187
xy <1927305259@qq.com>
2026-04-22 13:43:29 +0800

    修改main.c格式

--- main.c
+++ main.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
 int main(void) {
-  int a;
-  scanf("%d", &a);
-  int count = 0;
-  int end = a + 3;
-  for (int i = a; i <= end; i++) {
-    for (int j = a; j <= end; j++) {
-      for (int k = a; k <= end; k++) {
-        if (i != j && i != k && j != k) {
-          count++;
-          printf("%d%c", i * 100 + j * 10 + k, (count % 6 == 0 ? '\n' : ' '));
-        }
-      }
-    }
-  }
-  return 0;
+	int a;
+	scanf("%d", &a);
+	int end = a + 3;
+	int count = 0;
+
+	for (int i = a; i <= end; i++) {
+		for (int j = a; j <= end; j++) {
+			for (int k = a; k <= end; k++) {
+				if (i != j && i != k && k != j) {
+					++count;
+					printf("%d%c", i * 100 + j * 10 + k,
+							(count % 6 == 0 ? '\n' : ' '));
+							}
+			}
+		}
+	}
 }
