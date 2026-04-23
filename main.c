3936e64e3bfedc7fb6d9c182cdf855730a14480a
xy <1927305259@qq.com>
2026-04-23 21:36:41 +0800

    素数之和,素数次数过样例了

--- main.c
+++ main.c
@@ -4,13 +4,18 @@ int main(void) {
 	int m, n;
 	scanf("%d %d", &m, &n);
 	int cnt = 0, sum = 0;
+	int flag;
 
 	for (int i = m; i <= n; i++) {
+		int flag = 0;
 		for (int j = 2; j * j <= i; j++) {
 			if (i % j == 0) {
-				sum += i;
-				++cnt;
+				flag = 1;
 			}
+		}
+		if (!flag) {
+			++cnt;
+			sum += i;
 		}
 	}
 
