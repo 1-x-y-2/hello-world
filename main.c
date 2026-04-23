04b195b1c98194ca838193be99766c013ad81fcf
xy <1927305259@qq.com>
2026-04-23 21:29:45 +0800

    素数和和个数

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
 int main(void) {
-	printf("Hi");
+	int m, n;
+	scanf("%d %d", &m, &n);
+	int cnt = 0, sum = 0;
+
+	for (int i = m; i <= n; i++) {
+		for (int j = 2; j * j <= i; j++) {
+			if (i % j == 0) {
+				sum += i;
+				++cnt;
+			}
+		}
+	}
+
+	printf("%d %d\n", cnt, sum);
 	return 0;
 }
