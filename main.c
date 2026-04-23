5e2f87ad93737196b97d8ac4108314e0e579015e
xy <1927305259@qq.com>
2026-04-23 19:10:52 +0800

    测试

--- main.c
+++ main.c
@@ -1,17 +1,6 @@
 #include <stdio.h>
 
 int main(void) {
-<<<<<<< HEAD
-=======
-	int n;
-	scanf("%d", &n);
-
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= i; j++) {
-			printf("%dx%d=%-2d ", j, i, i * j);
-		}
-		printf("\n");
-	}
->>>>>>> ee5099f (修改main.c)
+	printf("Hi");
 	return 0;
 }
