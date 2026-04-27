faf06448aeb4a4067edef5e3f9a16a00251ff603
y <x>
2026-04-27 17:09:40 +0800

    xogame

--- main.c
+++ main.c
@@ -23,6 +23,7 @@ int main() {
   int y = 0;
   int z = 0;
   int f = 0;
+  int flag = 0;
   for (int i = 0, e = 2; i < 3; i++, e--) {
     z += g[i][i];
     f += g[i][e];
@@ -30,18 +31,26 @@ int main() {
       x += g[i][j];
       y += g[j][i];
     }
-    if (judge(x) == 1 || judge(y) == 0)
+    if (judge(x) || judge(y)) {
+      flag = 1;
       break;
+    }
     x = 0;
     y = 0;
   }
+  if (flag != 1 && (judge(z) || judge(f)))
+    ;
   return 0;
 }
+
 int judge(int n) {
   if (n == 3) {
     printf("X");
     return 1;
   } else if (n == 0) {
-    
+    printf("O");
+    return 1;
+  } else {
+    return 0;
   }
 }
