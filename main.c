28f8a5dcefdbf0842dca480b5e5399562c6ee784
xy <1927305259@qq.com>
2026-04-28 07:43:57 +0800

    学习代码

--- main.c
+++ main.c
@@ -1,56 +1,15 @@
 #include <stdio.h>
 
-int judge(int n);
+void swap(int *a, int *b) {
+  int t = *a;
+  *a = *b;
+  *b = t;
+}
 int main() {
-  int g[3][3];
-  int n;
-
-  for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 3; j++) {
-      scanf("%d", &n);
-      g[i][j] = n;
-    }
-  }
-
-  for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 3; j++) {
-      printf("%d ", g[i][j]);
-    }
-    printf("\n");
-  }
-
-  int x = 0;
-  int y = 0;
-  int z = 0;
-  int f = 0;
-  int flag = 0;
-  for (int i = 0, e = 2; i < 3; i++, e--) {
-    z += g[i][i];
-    f += g[i][e];
-    for (int j = 0; j < 3; j++) {      
-      x += g[i][j];
-      y += g[j][i];
-    }
-    if (judge(x) || judge(y)) {
-      flag = 1;
-      break;
-    }
-    x = 0;
-    y = 0;
-  }
-  if (flag != 1 && (judge(z) || judge(f)))
-    ;
+  int x = 1;
+  int y = 2;
+  swap(&x, &y);
+  printf("x=%d,y=%d\n", x, y);
   return 0;
 }
 
-int judge(int n) {
-  if (n == 3) {
-    printf("X");
-    return 1;
-  } else if (n == 0) {
-    printf("O");
-    return 1;
-  } else {
-    return 0;
-  }
-}
